987460f96696974707fd64bb4c643d81099edc5a
Adina Sandoiu <102686335+adinna09@users.noreply.github.com>
2022-11-13 17:06:07 +0200

    new push

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <div>
-        {/* <Navbar/> */}
-        <Test/>
+        <Navbar/>
+        {/* <Test/> */}
     </div>
    
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,33 @@
-import { Typography} from "@mui/material";
-import React from "react";
-import { useState } from "react";
+import * as React from 'react';
+    import AppBar from '@mui/material/AppBar';
+    import Box from '@mui/material/Box';
+    import Toolbar from '@mui/material/Toolbar';
+    import Typography from '@mui/material/Typography';
+    import Button from '@mui/material/Button';
+    import IconButton from '@mui/material/IconButton';
+    import MenuIcon from '@mui/icons-material/Menu';
+    import poza from './imag/poza.png';
 
 
-function Navbar(props) {
-    const[name,setName] = useState('')
-    const handleNameChange = () => {
-        const names = ['ROXI', 'ADINA', 'DAN'];
-        const int = Math.floor(Math.random() * 3);
-        setName(names[int]);
+    
+  function CustomAppBar() {
+      return (
+        <Box style={{ width:'100vw',
+        height:'25vh'}}>
+          <AppBar position="static">
+            <Toolbar style={{backgroundColor:'#00BCD4'}}>
+              <img src={poza} style={{ width:'7vw'
+                ,height:'8vh'}} alt="poza" />
+                <Button style={{top:12.5, right:'40vw', position:'absolute', color:'#01579B'}} variant='contained'>PRET</Button>
+                <Button style={{top:12.5, right:'30vw', position:'absolute', color:'#01579B'}} variant='contained'>CONTACT</Button>
+                <Typography style={{top:19.5, right:'1vw', position:'absolute', color:'white'}} >
+                  Bine ati venit, Adina!
+                </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
     }
- 
-    return (
-        <div>
-            <div style={{justifyContent:'center', width:'40vw', height:'10vh', display:'flex'}}>
-                <button onClick={handleNameChange} style={{backgroundColor:'orange'}}>{name}</button>
-            </div>
-            <div style={{width:'100vw', height:'40vh'}}>
-                <Typography>bbbb</Typography>
-            </div>
-        </div>
-    )}
+    
 
-export default Navbar;
+export default CustomAppBar;
