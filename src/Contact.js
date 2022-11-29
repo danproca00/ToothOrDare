88ca664f4b78668eb6fe834011d38590362c6e26
Dan Proca <dan.proca00@e-uvt.ro>
2022-11-30 00:16:09 +0200

    backround added to contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
 import * as React from 'react';
+import imageBack from './imag/Background2.jpeg'
 
 
 
@@ -7,9 +8,12 @@ import * as React from 'react';
 
 function Contact() {
     return(
+        <div style={{ backgroundImage: `url(${imageBack})`, backgroundRepeat:'no-repeat', backgroundSize:'cover' , height:'92vh',  backgroundPosition:'center'}}>
         <Typography style={{width:'45vw', height:'50vh', justifyContent: 'center'}}>
             aici vor fi contacte
         </Typography>
+        </div>
+        
     ) 
 }
 
