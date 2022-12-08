2cdd3cf1f66b9c0679a4ce230aa5ce15b6dc9836
roxxie22 <117391736+roxxie22@users.noreply.github.com>
2022-12-08 12:30:01 +0200

    price update

--- src/Price.js
+++ src/Price.js
@@ -8,25 +8,35 @@ import * as React from 'react';
 function Price() {
     return(
         <div style={{width:'100vw', height:'100vh'}}>
-            <div style={{width:'100vw', height:'25vh', display:'flex', flexDirection:'row'}}>
-                <div style={{backgroundColor:'#00BCD4', height:'20vh', width:'20vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh'}}>
+            <div style={{width:'97vw', height:'50vh', display:'flex', flexDirection:'row'}}>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'9vh', borderRadius: 4}}>
                     <div style={{justifyContent:'center', display:'flex'}}>
                         <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
                     </div>
                 </div>
-                <div style={{backgroundColor:'#00BCD4', height:'20vh', width:'20vw',justifyContent:'space-between', marginLeft:'20vw', marginTop:'2vh'}}>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'9vh', borderRadius: 4}}>
+                    <div style={{justifyContent:'center', display:'flex'}}>
+                        <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
+                    </div>
+                </div>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'9vh', borderRadius: 4}}>
                     <div style={{justifyContent:'center', display:'flex'}}>
                         <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
                     </div>
                 </div>
             </div>
-            <div style={{width:'100vw', height:'30vh', display:'flex', flexDirection:'row'}}>
-                <div style={{backgroundColor:'#00BCD4', height:'20vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh'}}>
+            <div style={{width:'97vw', height:'50vh', display:'flex', flexDirection:'row'}}>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh', borderRadius: 4}}>
                     <div style={{justifyContent:'center', display:'flex'}}>
                         <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
                     </div>
                 </div>
-                <div style={{backgroundColor:'#00BCD4', height:'20vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh'}}>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh', borderRadius: 4}}>
+                    <div style={{justifyContent:'center', display:'flex'}}>
+                        <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
+                    </div>
+                </div>
+                <div style={{backgroundColor:'#00BCD4', height:'30vh', width:'30vw',justifyContent:'space-between', marginLeft:'3vw', marginTop:'2vh', borderRadius: 4}}>
                     <div style={{justifyContent:'center', display:'flex'}}>
                         <Typography style={{fontFamily:'boldmonospace',color:'#01579B', fontSize:'3vh' }}>DETALII</Typography>
                     </div>
